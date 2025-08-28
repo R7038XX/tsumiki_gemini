7546e8a79c7fd04dcf9b6dd183981e251a1d6654
MASAKING <R7038XX@users.noreply.github.com>
2025-08-28 21:52:37 +0900

    Update gitignore.tsx

--- src/commands/gitignore.tsx
+++ src/commands/gitignore.tsx
@@ -35,7 +35,7 @@ const GitignoreComponent: React.FC = () => {
         // commandsディレクトリ内のすべての.mdファイルと.shファイルを取得
         const files = await fs.readdir(tsumikiDir);
         const targetFiles = files.filter(
-          (file) => file.endsWith(".toml") || file.endsWith(".sh"),
+          (file) => file.endsWith(".toml") || file.endsWith(".sh") || file.endsWith(".md"),
         );
 
         // 具体的なファイルパスをルールとして作成
